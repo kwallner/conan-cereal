c3dcd5557f3d58713f198f3f5b1025824631cdac
Pedro Vicente <pedrovfer@gmail.com>
2018-02-07 14:45:56 +0000

    Added homepage and remove pthreads from libs as discussed with memshared.

--- conanfile.py
+++ conanfile.py
@@ -6,16 +6,16 @@ class CerealConan(ConanFile):
     name = "cereal"
     version = "1.2.2"
     description = "Serialization header-only library for C++11."
+    homepage = "https://github.com/USCiLab/cereal"
     license = "BSD-3"
     no_copy_source = True
     options = {"thread_safe": [True, False]}
     default_options = "thread_safe=False"
     source_subfolder = "sources"
-    settings = "os"
-    url = "https://github.com/USCiLab/cereal"
+    url = "https://github.com/conan-community/conan-cereal"
 
     def source(self):
-        source_url = ("%s/archive/v%s.zip" % (self.url, self.version))
+        source_url = ("%s/archive/v%s.zip" % (self.homepage, self.version))
         tools.get(source_url)
         os.rename("%s-%s" % (self.name, self.version, ), self.source_subfolder)
 
@@ -29,6 +29,5 @@ class CerealConan(ConanFile):
         self.info.header_only()
 
     def package_info(self):
-        if self.options.thread_safe and self.settings.os != "Windows":
+        if self.options.thread_safe:
             self.cpp_info.defines = ["CEREAL_THREAD_SAFE=1"]
-            self.cpp_info.libs = ["pthread"]
