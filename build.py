5f4c30bb1e81a3f31a3f6fa4a990f6d81c88408e
Pedro Vicente <pedrovfer@gmail.com>
2018-02-08 13:09:52 +0000

    Updated ci to run with gcc 5

--- build.py
+++ build.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     builder = ConanMultiPackager(use_docker=True)
     builder.add(settings={
         'os': 'Linux',
-        'compiler.version': '7',
+        'compiler.version': '5',
         'compiler.libcxx': 'libstdc++11',
         'arch': 'x86_64',
         'build_type': 'Release',
